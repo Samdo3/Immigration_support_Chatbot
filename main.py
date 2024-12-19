e88d9360fe41c3272d8b0af60f0662ed8d5b90bd
Samdo3 <clcleh123@gmail.com>
2024-12-20 00:42:49 +0900

    fast api

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hello world!")
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello from FastAPI on Azure App Service"}
